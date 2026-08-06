25130f9a99e049a8d565ea5af12efaf182a82df9
AdityaGalgale <adityagalgale09@gmail.com>
2026-08-06 23:56:07 +0530

    Floor and ParkingSlot service added

---
+++ backend/smart-parking-system/src/main/java/com/smartparking/service/ParkingSlotService.java
@@ -0,0 +1,19 @@
+package com.smartparking.service;
+
+import java.util.List;
+
+import com.smartparking.dto.response.ParkingSlotResponse;
+
+public interface ParkingSlotService {
+
+    ParkingSlotResponse getSlotById(Long slotId);
+
+    List<ParkingSlotResponse> getSlotsByFloor(Long floorId);
+
+    List<ParkingSlotResponse> getAvailableSlots();
+    
+    List<ParkingSlotResponse> getAvailableSlotsByProperty(Long propertyId);
+    
+    List<ParkingSlotResponse> getSlotsByProperty(Long propertyId);
+
+}
